9b4f2986403f64bc114cf6940544ad1fbc13435d
inuwr19 <yusufaminu19@gmail.com>
2024-05-31 14:04:21 +0700

    Create Model & Migration

---
+++ app/Models/Order.php
@@ -0,0 +1,23 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\Relations\BelongsTo;
+use Illuminate\Database\Eloquent\Relations\HasMany;
+
+class Order extends Model
+{
+    use HasFactory;
+
+    public function customer() : HasMany
+    {
+        return $this->hasMany(Customer::class);
+    }
+
+    public function postOrder() : BelongsTo
+    {
+        return $this->belongsTo(Customer::class);
+    }
+}
